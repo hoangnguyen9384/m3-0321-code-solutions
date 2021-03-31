c875cabd3a107c37a8eedcbeaba774c5f6494bd5
hoangnguyen9384 <ng_viethoang@yahoo.com>
2021-03-31 03:57:09 +0000

    update for change request

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,56 +1,45 @@
-import React from "react";
+import React from 'react';
 
 export default class ValidatedInput extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      password: ""
+      password: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event){
-    this.setState({password: event.target.value})
+  handleChange(event) {
+    this.setState({ password: event.target.value });
   }
 
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
   }
 
-  render(){
+  render() {
     const input = this.state.password;
+    let message = '';
+    let icon = 'fa-user-check';
+
     if (!input) {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <h2>Password</h2>
-          <input type="text" value={this.state.password} onChange={this.handleChange}/>
-          <span>
-            <i className="x-cross" class="fas fa-user-times"></i>
-          </span>
-          <p className="message">A password is required</p>
-        </form>
-      );
-    } else if ( input.length < 8){
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <h2>Password</h2>
-          <input type="text" value={this.state.password} onChange={this.handleChange} />
-          <span>
-            <i className="x-cross" class="fas fa-user-times"></i>
-          </span>
-          <p className="message">Your password is too short</p>
-        </form>
-      );
-    } else {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <h2>Password</h2>
-          <input type="text" value={this.state.password} onChange={this.handleChange} />
-          <span>
-            <i class="fas fa-user-check"></i>
-          </span>
-        </form>);
+      icon = 'fa-user-times';
+      message = 'A password is required';
+    } else if (input.length < 8) {
+      icon = 'fa-user-times';
+      message = 'Your passowrd is too short';
     }
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <h2>Password</h2>
+          <input type="text" value={this.state.password} onChange={this.handleChange} />
+          <span>
+            <i className={`fas ${icon}`}></i>
+          </span>
+          <p className="message">{message}</p>
+        </form>
+
+    );
   }
 }
