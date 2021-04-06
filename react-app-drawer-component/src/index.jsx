daf352d93d211d975283f66e93aa60705c24471c
hoangnguyen9384 <ng_viethoang@yahoo.com>
2021-04-06 14:06:15 +0000

    complete react app-drawer

--- react-app-drawer-component/src/index.jsx
+++ react-app-drawer-component/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Appdrawer from './app-drawer';
+import AppDrawer from './app-drawer';
 
 ReactDOM.render(
-  <Appdrawer />,
+  <AppDrawer />,
   document.getElementById('root')
 );
